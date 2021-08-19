791cbe0b5f03f23f0f873b749aea283043a67879
Emam96 <emamsh990@gmail.com>
2021-08-19 13:10:23 +0300

    Done with memory / lab 10

--- data/movie.js
+++ data/movie.js
@@ -1,34 +1,45 @@
-const axios = require('axios'); 
-module.exports = getMovieHandler; 
+const axios = require("axios");
+module.exports = getMovieHandler;
+
+let inMemory = {};
 
 // https://api.themoviedb.org/3/search/movie?api_key=&language=en-US&query=amman&include_adult=false
 
 function getMovieHandler(req, res) {
-    let searchQuery = req.query.searchQuery;
-    // https://api.themoviedb.org/3/search/movie?api_key=&language=en-US&query=amman&include_adult=false
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${searchQuery}&include_adult=false`;
-  
+  let searchQuery = req.query.searchQuery;
+  // https://api.themoviedb.org/3/search/movie?api_key=&language=en-US&query=amman&include_adult=false
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${searchQuery}&include_adult=false`;
+
+  if (inMemory[searchQuery] !== undefined) {
+    console.log(" cache hit / MOVIES ");
+    res.send(inMemory[searchQuery]);
+  } else {
+    console.log(" cache miss / MOVIES ");
+
     try {
       axios.get(url).then((searchResults) => {
-        console.log(searchResults.data.results);
-  
+        // console.log(searchResults.data.results);
+
         let movieArray = searchResults.data.results.map((city) => {
           return new Movie(city);
         });
-  
+
+        inMemory[searchQuery] = movieArray;
+
         res.send(movieArray);
       });
     } catch (error) {
       console.log("error from axios", error);
       res.send(error);
     }
-  
+
     console.dir(url);
   }
-  
+
   class Movie {
     constructor(cityData) {
       this.title = cityData.title;
-      this.poster = `https://image.tmdb.org/t/p/original/${cityData.poster_path}`;                        //  cityData.poster_path; 
+      this.poster = `https://image.tmdb.org/t/p/original/${cityData.poster_path}`; //  cityData.poster_path;
     }
   }
+}
--- data/weather.js
+++ data/weather.js
@@ -1,13 +1,22 @@
 const axios = require('axios'); 
 module.exports = getForecastHandler; 
 
-
+let inMemory = {};
 
 function getForecastHandler(req, res) {
     let searchQuery = req.query.searchQuery;
     // https://api.weatherbit.io/v2.0/forecast/daily?city=amman&key=
     let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQuery}&key=${process.env.WEATHER_API_KEY}`;
   
+
+    if (inMemory[searchQuery] !== undefined) {
+      console.log(" cache hit / WEATHER ");
+      res.send(inMemory[searchQuery]);
+    } else {
+      console.log(" cache miss / WEATHER ");
+
+
+
     try {
       axios.get(url).then((searchResults) => {
         // console.log(searchResults.data.data);
@@ -16,6 +25,8 @@ function getForecastHandler(req, res) {
           return new City(city);
         });
   
+        inMemory[searchQuery] = forecastArray;
+
         res.send(forecastArray);
       });
     } catch (error) {
@@ -32,3 +43,4 @@ function getForecastHandler(req, res) {
       this.date = cityData.valid_date;
     }
   }
+}
